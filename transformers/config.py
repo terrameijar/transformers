1bfe3f8763a1c56db99a1f33a9a4c3135eaab97f
Vuyisile Ndlovu <terrameijar@gmail.com>
2024-11-06 07:05:19 +0200

    Tweak DB connection string

--- transformers/config.py
+++ transformers/config.py
@@ -6,10 +6,24 @@ load_dotenv()
 
 class Config:
     basedir = os.path.abspath(os.path.dirname(__file__))
-    SQLALCHEMY_DATABASE_URI = os.getenv(
-        "SQLALCHEMY_DATABASE_URI"
-    ) or "sqlite:///" + os.path.join(basedir, "transformers.db")
+
+    # Read Environment Variables
+
     SQLALCHEMY_TRACK_MODIFICATIONS = (
         os.getenv("SQLALCHEMY_TRACK_MODIFICATIONS") or False
     )
+
+    POSTGRES_USER = os.getenv("POSTGRES_USER", "postgres")
+    POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+    POSTGRES_DB = os.getenv("POSTGRES_DB", "transformers")
+
+    if POSTGRES_PASSWORD:
+        SQLALCHEMY_DATABASE_URI = (
+            f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@db:5432/{POSTGRES_DB}"
+        )
+    else:
+        SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(
+            basedir, "transformers.db"
+        )
+
     SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
