013ef3bf6637671eeaa169b701ef5f4f592d9fb4
Vuyisile Ndlovu <terrameijar@gmail.com>
2025-02-26 12:36:25 +0200

    Add Query to check if database is populated before attempting to seed it.

--- transformers/commands.py
+++ transformers/commands.py
@@ -11,23 +11,26 @@ csv_file_path = "data/transformers.csv"
 @click.command(name="populate_db")
 @with_appcontext
 def populate_database():
-    with open(csv_file_path, mode="r", newline="", encoding="utf-8") as csv_file:
-        reader = csv.DictReader(csv_file)
+    if Transformer.query.count() == 0:
+        with open(csv_file_path, mode="r", newline="", encoding="utf-8") as csv_file:
+            reader = csv.DictReader(csv_file)
 
-        for row in reader:
-            transformer = Transformer(
-                name=row["Name"],
-                affiliation=row["Affiliation"],
-                abilities=row["Abilities"],
-                transformation_mode=row["Transformation/Alternate Mode"],
-                image_url=row["Image URL"],
-                description=row["Description"],
-                quote=row["Quote"],
-            )
-            db.session.add(transformer)
-        db.session.commit()
+            for row in reader:
+                transformer = Transformer(
+                    name=row["Name"],
+                    affiliation=row["Affiliation"],
+                    abilities=row["Abilities"],
+                    transformation_mode=row["Transformation/Alternate Mode"],
+                    image_url=row["Image URL"],
+                    description=row["Description"],
+                    quote=row["Quote"],
+                )
+                db.session.add(transformer)
+            db.session.commit()
+        print("Transformers data populated into database")
 
-    print("Transformers data populated into database")
+    else:
+        print("Database not empty.")
 
 
 @click.command(name="delete_duplicates")
